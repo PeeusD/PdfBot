622fa9ff7753f9158eb25d8f4b8dd31aba2614a9
PeeusD <2014pgmca076@nitjsr.ac.in>
2021-07-05 02:47:28 +0530

    added better handling

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from dotenv import load_dotenv
 import PyPDF2 as pd
 import re
-
+from time import sleep
 load_dotenv()
 
 TOKEN = getenv('TOKEN')
@@ -17,12 +17,16 @@ def start(update, context):
 def pdf_mgmt (update, context) :
         
     try:   
-        update.message.reply_text("***Processing***")
+        
+        message = update.message.reply_text("***Processing...and...Searching***")
+        
         file_id = update.message.document.file_id  #getting file id
         fileName = update.message.document.file_name   #getting filename
         newFile = bot.get_file(file_id)
         newFile.download()
+
         
+
         pattern = 'DAILY NEWSPAPERS PDF'
        
         dir_path = path.dirname(path.realpath(__file__))
@@ -36,8 +40,8 @@ def pdf_mgmt (update, context) :
 
         infile = pd.PdfFileReader(fileName, 'rb')
         numPages = infile.getNumPages()
-        context.bot.send_chat_action(chat_id=update.effective_message.chat_id, action=ChatAction.TYPING)
-        update.message.reply_text("Searching...")
+        
+        
         delPages = []
 
         for i in range(0, numPages):
@@ -50,6 +54,10 @@ def pdf_mgmt (update, context) :
                 delPages.append(i)
 
         context.bot.send_chat_action(chat_id=update.effective_message.chat_id, action=ChatAction.TYPING)
+        
+        message_id = message.message_id
+        sleep(3)
+        bot.delete_message(chat_id=update.effective_message.chat_id, message_id= message_id)
         update.message.reply_text(f'Pattern found in Page nos.: {delPages}')
 
         if len(delPages) > 0 :
@@ -63,7 +71,7 @@ def pdf_mgmt (update, context) :
 
             with open(fileName, 'wb') as f:
                 output.write(f)
-            context.bot.send_chat_action(chat_id=update.effective_message.chat_id, action=ChatAction.TYPING)
+          
             update.message.reply_text(f'{delPages} Pages have been deleted!')
             #uploading...
             context.bot.send_chat_action(chat_id=update.effective_message.chat_id, action=ChatAction.UPLOAD_DOCUMENT)
@@ -74,7 +82,7 @@ def pdf_mgmt (update, context) :
             update.message.reply_text(f"Word: '{pattern}' not found in PDF!")
         remove(fileName)    
     except Exception as e:
-        print(e)
+        update.message.reply_text(e)
           
     
 
