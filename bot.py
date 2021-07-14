0ef470301e603a496a2aff670a824903a8b365bb
PeeusD <2014pgmca076@nitjsr.ac.in>
2021-07-14 10:40:57 +0530

    updated reading file type

--- bot.py
+++ bot.py
@@ -43,8 +43,8 @@ def pdf_mgmt (update, context) :
 
         # merging promo pdfs to  regular pdfs
         merger = pd.PdfFileMerger()
-        merger.append(pd.PdfFileReader('promo.pdf', strict = False))
-        merger.append(pd.PdfFileReader(fileName, strict = False))
+        merger.append(pd.PdfFileReader('promo.pdf', 'rb'))
+        merger.append(pd.PdfFileReader(fileName, 'rb'))
         merger.write(fileName)
         merger.close()
 
