4b7da014697c8f66ef2be5d70b29354ecc4cfed4
PeeusD <2014pgmca076@nitjsr.ac.in>
2021-07-16 01:33:21 +0530

    finally fixed-- os path

--- bot.py
+++ bot.py
@@ -95,16 +95,17 @@ def pdf_mgmt (update, context) :
                         #uploading...
                         context.bot.send_chat_action(chat_id=update.effective_message.chat_id, action=ChatAction.UPLOAD_DOCUMENT)
                         #For debugging use update eff....
-                        context.bot.send_document(chat_id=update.effective_message.chat_id, document=open(fileName, 'rb'), timeout=240)
+                        context.bot.send_document(chat_id=CHANNEL_ID, document=open(fileName, 'rb'), timeout=240)
 
                     else:
                         context.bot.send_chat_action(chat_id=update.effective_message.chat_id, action=ChatAction.TYPING)
                         update.message.reply_text(f"Word: '{pattern}' not found in PDF!")
                         context.bot.send_chat_action(chat_id=update.effective_message.chat_id, action=ChatAction.UPLOAD_DOCUMENT)
                         #For debugging use update eff...
-                        context.bot.send_document(chat_id=update.effective_message.chat_id, document=open(fileName, 'rb'), timeout=240)
+                        context.bot.send_document(chat_id=CHANNEL_ID, document=open(fileName, 'rb'), timeout=240)
                     
                     remove(path.join(root,fileName))  #delting pdf from directory
+                    print('File deleted!')
                     
     except Exception as e:
        print(e)
